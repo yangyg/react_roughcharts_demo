15e2837fd71d20d24ec9f7a74ff2b8986eb808e5
yyg <yonggan.yang@shinyinfo.com.cn>
2019-11-07 09:29:15 +0800

    roughchart

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Chart from './chart'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+
+        <Chart/>
+
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -19,6 +23,7 @@ function App() {
           Learn React
         </a>
       </header>
+
     </div>
   );
 }
